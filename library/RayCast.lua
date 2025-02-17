d8e27d45c6698956e03ad06e07138abc5b699336
SeanTheBuilder1 <seanthebuilder52@gmail.com>
2025-02-17 10:33:44 +0800

    feat: add raycasting of rigid bodies

---
+++ library/RayCast.lua
@@ -0,0 +1,12 @@
+---@meta
+
+---@module "Vector"
+---@module "Node"
+
+---@param origin Vector3
+---@param direction Vector3
+---@param distance number
+---@return Node?
+function shootRay(origin, direction, distance) end
+
+return shootRay
